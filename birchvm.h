ffe63ba28e7ed3a5a4e86d414d6c9a65e1d02ce2
Felipe Panosso <felipepanosso21@gmail.com>
2025-02-25 15:07:40 -0300

    Adiciona estrutura básica da CPU e mapeamento de instruções.

--- birchvm.h
+++ birchvm.h
@@ -18,4 +18,73 @@ typedef unsigned long long int int64;
 #define $c (char *)
 #define $i (int)
 
+/*
+    16 bit
+        AX
+        BX
+        CX
+        DX
+        SP
+        IP
+    65 KB memory
+    (Serial COM port)
+    (Floppy drive)
+*/
+typedef unsigned short int Reg;
+struct s_registers {
+    Reg ax;
+    Reg bx;
+    Reg cx;
+    Reg dx;
+    Reg sp;
+    Reg ip;
+};
+typedef struct s_registers Registers;
+
+struct s_cpu {
+    Registers r;
+};
+typedef struct s_cpu CPU;
+
+/*
+    mov ax, 0x05 // (0x01 OR ax)
+                 // 0000 0011 = mov
+                 // 0000 0000
+                 // 000 0000 = 0x05
+*/
+
+enum e_opcode {
+    mov = 0x01, 
+    nop = 0x02
+};
+typedef enum e_opcode Opcode;
+
+struct s_instrmap {
+    Opcode o;
+    int8 size;
+};
+typedef struct s_intrmap IM;
+
+typedef int8 Args;
+struct s_instruction {
+    Opcode o;
+    Args a[]; /* 0-2 bytes */
+};
+typedef struct s_instruction Instruction;
+
+typedef int8 Stack[((unsigned int)(-1))];
+typedef Instruction Program;
+
+struct s_vm {
+     CPU c;
+     Program *p;
+};
+typedef struct s_vm VM;
+
+static Opcode opc;
+static IM instrmap[] = {
+    { mov, 0x03 },
+    { nop, 0x01 }
+};
+
 int main(int,char**);
